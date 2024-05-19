294533498d7934e53e856dc9ea0f76ec405d3061
whynot <anil@panix.com>
2024-05-18 22:29:30 -0400

    ReAdded SpringBoot

--- code/SBF/SpringBoot/src/test/java/ttl/larku/db/JPADemo.java
+++ code/SBF/SpringBoot/src/test/java/ttl/larku/db/JPADemo.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ttl.larku.domain.Course;
 import ttl.larku.domain.PhoneNumber;
-import ttl.larku.domain.ScheduledClass;
 import ttl.larku.domain.Student;
 
 import static java.lang.System.out;
@@ -77,32 +76,15 @@ public class JPADemo {
    }
 
    @Test
-   public void dumpScheduledClasses() {
-      try (EntityManager manager = emf.createEntityManager();) {
-
-//         TypedQuery<ScheduledClass> query =
-//               manager.createQuery("select sc from ScheduledClass sc", ScheduledClass.class);
-
-         TypedQuery<ScheduledClass> query =
-               manager.createQuery("select sc from ScheduledClass sc join fetch sc.course", ScheduledClass.class);
-
-         List<ScheduledClass> result = query.getResultList();
-
-         out.println("num : " + result.size());
-         result.forEach(out::println);
-      }
-
-   }
-
-   @Test
    public void dumpAllStudents() {
+      List<Student> result;
       try (EntityManager manager = emf.createEntityManager();) {
 
 //         TypedQuery<Student> query = manager.createQuery("select s from Student s", Student.class);
          TypedQuery<Student> query =
                manager.createQuery("select s from Student s left join fetch s.phoneNumbers", Student.class);
 
-         List<Student> result = query.getResultList();
+         result = query.getResultList();
 
          out.println("num : " + result.size());
          result.forEach(out::println);
