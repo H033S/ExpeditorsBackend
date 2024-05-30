3cfaeeeeddebd51aaa81c6aeb55911fe0e492b5f
whynot <anil@panix.com>
2024-05-30 12:02:11 -0400

    CourseRatingService security part 1

--- code/BackEnd/CourseRatingService/src/main/java/org/ttl/ratingservice/controller/CourseRatingServiceController.java
+++ code/BackEnd/CourseRatingService/src/main/java/org/ttl/ratingservice/controller/CourseRatingServiceController.java
@@ -3,21 +3,24 @@ package org.ttl.ratingservice.controller;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.ttl.ratingservice.service.CourseRatingService;
 
 @RestController
 @RequestMapping("/rating")
 public class CourseRatingServiceController {
 
-   private double lowerLimit = 1.0;
-   private double upperLimit  = 10.0;
+   @Autowired
+   private CourseRatingService service;
 
    @GetMapping("/{id}")
    public BigDecimal getRatingForCourse(@PathVariable("id") int id) {
-      var rd = ThreadLocalRandom.current().nextDouble(lowerLimit, upperLimit);
+      var rd = ThreadLocalRandom.current().nextDouble(service.getLowerLimit(), service.getUpperLimit());
       var rating = new BigDecimal(String.valueOf(rd)).setScale(1, RoundingMode.CEILING);
 
       return rating;
--- code/BackEnd/CourseRatingService/src/test/java/org/ttl/ratingservice/controller/CourseRatingControllerMvcTest.java
+++ code/BackEnd/CourseRatingService/src/test/java/org/ttl/ratingservice/controller/CourseRatingControllerMvcTest.java
@@ -5,13 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -22,7 +27,7 @@ public class CourseRatingControllerMvcTest {
    private MockMvc mockMvc;
 
    @Test
-   public void testGetRatingForCourse() throws Exception {
+   public void testGetRatingForCourseWithoutCredentialsGive401() throws Exception {
       int id = 1;
 
       MockHttpServletRequestBuilder builder = get("/rating/{id}", id)
@@ -30,14 +35,69 @@ public class CourseRatingControllerMvcTest {
             .contentType(MediaType.APPLICATION_JSON);
 
       ResultActions actions = mockMvc.perform(builder)
-            .andExpect(status().isOk());
+            .andExpect(status().isUnauthorized());
 
-      MvcResult result = actions.andReturn();
-
-      double rating = Double.parseDouble(result.getResponse().getContentAsString());
-
-      System.out.println("Rating: " + rating);
-      assertTrue(rating > 0);
+//      MvcResult result = actions.andReturn();
+//
+//      double rating = Double.parseDouble(result.getResponse().getContentAsString());
+//
+//      System.out.println("Rating: " + rating);
+//      assertTrue(rating > 0);
    }
 
+   @Test
+   @WithMockUser(roles = {"USER"})
+   public void testGetLimitsWithValidUserGives200() throws Exception {
+
+      var actions = mockMvc.perform(get("/admin/lowerLimit"))
+            .andExpect(status().isOk()).andReturn();
+
+      var ll = actions.getResponse().getContentAsString();
+
+      actions = mockMvc.perform(get("/admin/upperLimit"))
+            .andExpect(status().isOk()).andReturn();
+
+      var ul = actions.getResponse().getContentAsString();
+
+      System.out.println("ll: " + ll + ", ul: " + ul);
+   }
+
+
+   @Test
+   @WithMockUser(roles = {"USER"})
+   public void testSetLimitsWithInvalidUserGives403() throws Exception {
+
+      var actions = mockMvc.perform(put("/admin/lowerLimit/{ll}", 5.0))
+            .andExpect(status().isForbidden()).andReturn();
+
+//      actions = mockMvc.perform(put("/admin/upperLimit/{ul}", 35.0))
+//            .andExpect(status().isNoContent()).andReturn();
+//
+//      actions = mockMvc.perform(get("/admin/bothLimits"))
+//                  .andExpect(status().isOk()).andReturn();
+//
+//      var result = actions.getResponse().getContentAsString();
+//      System.out.println("result: " + result);
+//
+//      assertEquals("5.0:35.0", result);
+   }
+
+   @Test
+   @WithMockUser(roles = {"ADMIN"})
+   public void testSetLimitsWithUserGives200() throws Exception {
+
+      var actions = mockMvc.perform(put("/admin/lowerLimit/{ll}", 5.0))
+            .andExpect(status().isNoContent()).andReturn();
+
+      actions = mockMvc.perform(put("/admin/upperLimit/{ul}", 35.0))
+            .andExpect(status().isNoContent()).andReturn();
+
+      actions = mockMvc.perform(get("/admin/bothLimits"))
+                  .andExpect(status().isOk()).andReturn();
+
+      var result = actions.getResponse().getContentAsString();
+      System.out.println("result: " + result);
+
+      assertEquals("5.0:35.0", result);
+   }
 }
